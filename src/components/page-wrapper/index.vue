c02a4432790c9355016e41b5b0a2255d21db62b2
hanmiaomiao <18531151201@163.com>
2020-07-21 11:19:32 +0800

    页面包装器组件--包含页面进入离开的动画和当前页的z-index

---
+++ src/components/page-wrapper/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- 页面包装器 -->
+    <transition appear :leave-active-class="LeaveActive" :enter-active-class="EnterActive">
+        <section class="page-wrapper" :style="{'z-index':zIndexC}">
+            <slot></slot>
+        </section>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'page-wrapper',
+    props: {
+        LeavAction: {
+            type: String,
+            default: 'animated fadeOutRight' // 右 → 左
+        },
+        EnterActive: {
+            type: String,
+            default: 'animated fadeOutRight' // 右 → 左
+        },
+        zIndexC: {
+             // 多个可能的类型
+            type: [String, Number],
+            default: 2
+        }
+    },
+    data() {
+        return {}
+    },
+    filters: {},
+    computed: {},
+    mounted() { },
+    methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+    .page-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+</style>
