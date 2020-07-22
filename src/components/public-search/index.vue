2730e13ef24ee59886b3c53209e3d4061b0b9084
hanmiaomiao <18531151201@163.com>
2020-07-22 17:06:21 +0800

    封装搜索组件

---
+++ src/components/public-search/index.vue
@@ -0,0 +1,102 @@
+<template>
+<!-- 搜索组件 -->
+    <div class="search">
+        <div class="search-box">
+            <span class="search-icon">
+                <img src="../../assets/sousou.png" alt="">
+            </span>
+            <input type="text" :placeholder="placeholdertext" @input="onSearchInput" v-model.trim="value">
+            <span class="clear-icon">
+                <img src="../../assets/close.png" alt="" v-show='isShowClearIcon' @click="clearValue">
+            </span>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'search',
+    data() {
+        return {
+            value: ''
+        }
+    },
+    props: {
+        placeholdertext: {
+            type: String,
+            default: '请输入关键字' // 搜索框默认要显示的内容
+        },
+        userInput: { //用户输入数据
+            type: [String, Number],
+            default: ''
+        },
+        isShowClearIcon:{
+          type:Boolean,
+          default:false
+        }
+    },
+    watch: {
+        userInput(newVal, oldVal) {
+            this.value = newVal
+        }
+    },
+    filters: {},
+    computed: {},
+    mounted() {
+    },
+    methods: {
+        onSearchInput() {
+            this.$emit('onSearchInput', this.value)
+        },
+        // 清除输入框的内容
+        clearValue(){
+          this.$emit('clearValue')
+        }
+    }
+}
+</script>
+
+<style type='text/scss' lang='scss' scoped>
+.search {
+    width: 100%;
+    display: flex;
+    .search-box {
+        width: 90%;
+        height: 28px;
+        margin: 0 auto;
+        border-radius: 4px;
+        background: #fff;
+        display: flex;
+        > .search-icon {
+            height: 100%;
+            padding-left: 6px;
+            > img {
+                height: 20px;
+                margin-top: 4px;
+            }
+        }
+        > input {
+            width: 100%;
+            height: 100%;
+            padding-left: 5px;
+            padding-right: 5px;
+            color: #827f7f;
+            text-overflow: ellipsis;
+            > ::placeholder {
+                color: chocolate;
+            }
+        }
+        > .clear-icon {
+            height: 100%;
+            padding-right: 6px;
+
+            > img {
+                height: 20px;
+                margin-top: 4px;
+            }
+        }
+    }
+}
+</style>
--- src/views/help/index.vue
+++ src/views/help/index.vue
@@ -1,148 +1,56 @@
 <template>
     <div class="help">
-        <div class="header">
-            <div>帮助热线</div>
-            <div>
-                <img src="../../assets/tel@3x.png" alt="">
-                <a :href="'tel:' + '4008800114-6-4'">4008800114-6-4</a>
-            </div>
-        </div>
-        <div class="container">
-            <p>热点问题</p>
-            <div>
-                <p>注册后登录不进去怎么办</p>
-                <p>请确定后输入的数据正确请确定后输入的数据正确输
-                    入的数据正确请确定后输入的数据正确</p>
-            </div>
-            <div>
-                <p>注册后登录不进去怎么办</p>
-                <p>请确定后输入的数据正确请确定后输入的数据正确输
-                    入的数据正确请确定后输入的数据正确</p>
-            </div>
-            <div>
-                <p>注册后登录不进去怎么办</p>
-                <p>请确定后输入的数据正确请确定后输入的数据正确输
-                    入</p>
-            </div>
-        </div>
-        <div class="footer">
-            技术支持:中国石油管道公司信息中心
-        </div>
-        <div class="qxz-error-text" v-if="qxzIsErrorText">
-            {{qxzErrorText}}
-        </div>
+
+        <publicSearch :placeholdertext="placeholdertext" :isShowClearIcon="isShowClearIcon" @clearValue="clearValue"
+            @onSearchInput="onSearchInput" :userInput="userInput" class="search-box">
+        </publicSearch>
+
     </div>
 </template>
 
 <script>
+import publicSearch from '../../components/public-search/index'
 export default {
     name: '',
     data() {
         return {
+            userInput: '', //用户输入数据
+            placeholdertext: '请输入用户姓名/拼写',
+            isShowClearIcon: false //是否显示清除的icon
 
         }
     },
+    components: { publicSearch },
     filters: {},
     computed: {},
     mounted() {
-        // this.ShowToast('nihao')
-          // this.ShowLoading('qing liangjie')
-        //   const _that = this
-        //  setTimeout(()=>{
-        //     _that.HideLoading('111')
-        //  },1000)
-        // this.$_qxz_show_error_text()
-        // console.log(this.qxzErrorText,this.qxzIsErrorText,'qxzIsErrorText');
     },
-    methods: {}
+    methods: {
+        //获取用户输入的数据
+        onSearchInput(val) {
+            this.userInput = val.replace(/\s*/g, "") //// 去除 空格
+            console.log(this.userInput.length, 'this.userInput.length');
+            if (this.userInput.length) {
+                this.isShowClearIcon = true
+            }
+        },
+        //清除输入框内内容
+        clearValue() {
+            this.isShowClearIcon = !this.isShowClearIcon
+            this.userInput = ''
+        }
+    }
 }
 </script>
 
 <style type='text/scss' lang='scss' scoped>
 .help {
-    width: 100%;
-    height: 100%;
-    background: #f6f6f6;
     font-family: "PingFangSC-Medium";
     position: relative;
-    .header {
-        width: 100%;
-        height: 47px;
-        background: #fff;
-        display: flex;
-        justify-content: space-between;
+    .search-box {
+        background: chocolate;
         align-items: center;
-        padding: 0 24px 0 19px;
-
-        > div:nth-child(1) {
-            font-size: 17px;
-            color: #333333;
-            letter-spacing: -0.58px;
-        }
-        > div:nth-child(2) {
-            display: flex;
-            > img {
-                width: 21px;
-                height: 21px;
-                margin-right: 10px;
-            }
-            > a {
-                height: 21px;
-                line-height: 21px;
-                font-size: 16px;
-                color: #0859b5;
-                letter-spacing: -0.55px;
-                text-align: center;
-            }
-        }
-    }
-    .container {
-        width: 100%;
-        background: #fafafa;
-        > p {
-            font-size: 13px;
-            color: #757575;
-            letter-spacing: -0.44px;
-            line-height: 18px;
-            padding-top: 16px;
-            padding-left: 18.5px;
-            margin-bottom: 10px;
-        }
-        > div {
-            > p:nth-child(1) {
-                padding: 0 21px;
-                margin-bottom: 6px;
-                line-height: 22px;
-                font-size: 16px;
-                color: #333333;
-                letter-spacing: -0.55px;
-                font-weight: 600;
-            }
-            > p:nth-child(2) {
-                line-height: 21px;
-                padding: 0 21px 0 20px;
-                font-size: 15px;
-                color: #666666;
-                letter-spacing: -0.51px;
-                padding-bottom: 29px;
-            }
-        }
-    }
-    .footer {
-        width: 220px;
-        height: 20px;
-        font-size: 14px;
-        color: #757575;
-        letter-spacing: -0.48px;
-        line-height: 20px;
-        // text-align: center;
-        position: absolute;
-        bottom: 20px;
-        left: 50%;
-        margin-left: -110px;
+        height: 44px;
     }
 }
-// .qxz-error-text {
-//     top: 44px;
-// }
 </style>
