87901cd2bd0d0e5703db636fdb9a3558ab5b61ff
hanmiaomiao <18531151201@163.com>
2020-08-04 17:57:59 +0800

    封装多tab组件--两个以上

---
+++ src/components/multi-tabs/index.vue
@@ -0,0 +1,77 @@
+<template lang="html">
+    <div  class="sub-tab-ul">
+        <div  class="sub-tab-wrap">
+            <div class="sub-tab-li" :class="{selectCla:idx===selectIdx}" @click.stop="onClick(el,idx)" v-for="(el,idx) in cDataTitle" :key="idx">
+                    <i v-if="idx===selectIdx" :class="{selectClaI:idx===selectIdx}"></i>
+                {{el.title}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'multi-tabs',
+    props: {
+        cDataTitle: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    },
+    data() {
+        return {
+            selectIdx: 0,
+        };
+    },
+    mounted() { },
+    methods: {
+        onClick(el, val) {
+            this.selectIdx = val;
+            this.$emit('onClick', el, val);
+        },
+        onLoad() {
+
+        },
+    },
+};
+</script>
+
+<style lang="scss" type="text/scss" scoped>
+.selectCla {
+    color: #1e79ff;
+}
+.selectClaI {
+    border-bottom: 2px solid #1e79ff;
+}
+.sub-tab-ul::-webkit-scrollbar {
+    display: none;
+}
+.sub-tab-ul {
+    position: relative;
+    height: 40px;
+    line-height: 40px;
+    border-bottom: 1px solid #ccc;
+    overflow-x: scroll;
+    > .sub-tab-wrap {
+        position: absolute;
+        white-space: nowrap;
+        padding: 0 9px;
+        > div:last-child {
+            margin-right: 0px;
+        }
+        > .sub-tab-li {
+            position: relative;
+            display: inline-block;
+            margin-right: 10px;
+            > i {
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: 1px;
+            }
+        }
+    }
+}
+</style>
--- src/views/statistic/index.vue
+++ src/views/statistic/index.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="statistic">
-        statistic.....
+        <multiTabs :cDataTitle="list" @onClick="onClick"></multiTabs>
     </div>
 </template>
 
 <script>
+import multiTabs from '../../components/multi-tabs/index';
 export default {
     name: '',
     data() {
         return {
-
-
+            list: [
+                { title: '原料加工进厂' },
+                { title: '成品油产量' },
+                { title: '主要产品出厂及库存' },
+                { title: '主要产品出厂及库存' },
+            ],
         };
     },
+    components:{multiTabs},
     filters: {},
     computed: {},
     mounted() { },
-    methods: {}
+    methods: {
+        onClick(val){
+            console.log(val);
+        }
+    }
 };
 </script>
 
@@ -24,6 +34,5 @@ export default {
 .statistic {
     width: 100%;
     height: 100%;
-    background: rgb(153, 153, 153);
 }
 </style>
