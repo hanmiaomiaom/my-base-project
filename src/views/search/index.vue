48d911d08401f3c05229ed886234a32a2a1443cc
hanmiaomiao <18531151201@163.com>
2020-08-13 16:37:07 +0800

    封装轮播图

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,18 +1,40 @@
 <template>
     <div class="search">
-        <cTable class="tables"></cTable>
+        <!-- <cTable class="tables"></cTable> -->
+        <swiper :imgList="banners" style=" width:100%;height: 200px;"></swiper>
     </div>
 </template>
 
 <script>
-import cTable from './table';
+// import cTable from './table';
+// import swiper from '../../components/swiper/index';
+import swiper from '../../components/swiper/lunbo';
 export default {
     name: '',
     data() {
         return {
+            // imgList: [
+            //     {
+            //         imgURI:require('../../assets/1.png'),
+            //         commodityId: 40
+            //     },
+            //     {
+            //         imgURI: require('../../assets/3.png'),
+            //         commodityId: 16
+            //     },
+            //     {
+            //         imgURI: require('../../assets/4.png'),
+            //         commodityId: 35
+            //     },
+            // ],
+            banners: [
+                { image: require('../../assets/1.png') },
+                { image: require('../../assets/3.png') },
+                { image: require('../../assets/4.png') },
+            ]
         };
     },
-    components: { cTable },
+    components: { swiper },
     filters: {},
     computed: {},
     mounted() { },
@@ -24,9 +46,9 @@ export default {
 .search {
     width: 100%;
     height: 100%;
-    .tables {
-        height: 100%;
-        padding: 0 5px;
-    }
+    // .tables {
+    //     height: 100%;
+    //     padding: 0 5px;
+    // }
 }
 </style>
